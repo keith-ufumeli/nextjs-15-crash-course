167d13500f88363c6c6cfbf821ec04c9d165a727
Keith Ufumeli <ufumelik@gmail.com>
2025-08-18 13:58:40 +0200

    Refactor Home page to remove unused search functionality and streamline data fetching; update Sanity types to include new schema definitions and improve type safety.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard from "@/components/StartupCard";
 import { StartupTypeCard } from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
-import { STARTUPS_QUERY, STARTUPS_SEARCH_QUERY } from "@/sanity/lib/queries";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
   searchParams,
@@ -12,23 +12,11 @@ export default async function Home({
   const query = (await searchParams).query;
   console.log(`Search query: ${query}`);
 
-  const posts = query 
-    ? await client.fetch(STARTUPS_SEARCH_QUERY, { search: query })
-    : await client.fetch(STARTUPS_QUERY);
-  // console.log(JSON.stringify(posts, null, 2));
+  // const posts = query 
+  //   ? await client.fetch(STARTUPS_SEARCH_QUERY, { search: query })
+  //   : await client.fetch(STARTUPS_QUERY);
 
-  /* const posts = [
-    {
-      _createdAt: new Date().toISOString(),
-      views: 100,
-      author: {_id: "1", name: "John Doe"},
-      title: "Startup 1",
-      category: "Technology",
-      _id: "1",
-      image: "https://picsum.photos/200/300",
-      description: "Description 1",
-    },
-  ]; */
+  const posts = await client.fetch(STARTUPS_QUERY);
 
   return (
     <>
