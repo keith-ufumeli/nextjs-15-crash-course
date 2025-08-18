948b07768c95d4e16daa0dae3ca5c54c17444f69
Keith Ufumeli <ufumelik@gmail.com>
2025-08-18 15:57:36 +0200

    Update Next.js and package dependencies; modify configuration for improved markdown support and enhance data fetching in Startup page. Refactor View component to streamline imports and improve code clarity.

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -21,12 +21,14 @@ export const experimental_ppr = true;
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const [post, { select: editorPosts }] = await Promise.all([
+  const [post, playlist] = await Promise.all([
     client.fetch(STARTUP_BY_ID_QUERY, { id }),
     client.fetch(PLAYLIST_BY_SLUG_QUERY, {
       slug: "editor-picks-new",
     }),
   ]);
+
+  const editorPosts = playlist?.select || [];
 
   if (!post) return notFound();
 
